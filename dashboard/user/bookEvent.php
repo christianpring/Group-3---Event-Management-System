0aa4e953656472d8053868acaeee71dc57752611
Jaycee Dela Cruz <delacruzjaycee501@gmail.com>
2024-11-28 00:15:38 +0800

    Revert "fixed bookevent & its css"
    This reverts commit c819afbfe7894f57a244db0e626615646a4c43ea.

--- dashboard/user/bookEvent.php
+++ dashboard/user/bookEvent.php
@@ -80,8 +80,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Dashboard</title>
-    <link rel="stylesheet" href="../../src/css/bookEvent.css">
+    <title>Event Booking</title>
+    <link rel="stylesheet" href="../../src/css/user.css">
 </head>
 <body>
     <nav>
